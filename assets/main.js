5d769137a1ab5894f9e744b6703721e2dbae2e3d
Cadman <jcadiente505@gmail.com>
2018-07-28 02:16:08 -0400

    Working on Projects cards in said section

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+// PARALLAX FUNCTION
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+// SIDENAV FUNCTION
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
